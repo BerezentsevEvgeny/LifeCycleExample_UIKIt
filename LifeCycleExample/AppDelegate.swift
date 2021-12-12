3b6d82df640d3a50b77dcbed501c8ed5554b094b
BerezentesvEvgeny <jonn09@icloud.com>
2021-12-12 15:39:33 +0300

    Check

--- LifeCycleExample/AppDelegate.swift
+++ LifeCycleExample/AppDelegate.swift
@@ -10,15 +10,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print(#function)
         return true
     }
-
+    
+    
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -31,10 +29,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
+    
     func applicationWillTerminate(_ application: UIApplication) {
         print(#function)
     }
-
-
 }
 
